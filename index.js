f2e358117892ac20937270db727a7f94aaacf734
송하경 <songhagyeong@CIndy.local>
2021-07-01 16:33:46 +0900

    auth기능, 로그아웃기능 추가

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const {User} = require("./models/User")
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
+const {auth} = require('./middleware/auth')
 
 //application/x-www-form-urlencoded  이렇게 생긴 데이타를 분석해서 가져올 수 있게 해주는 것
 app.use(express.urlencoded({extended: true}))
@@ -21,12 +22,12 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World!~안녕하세요~'))
 
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     //회원가입 할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
     const user = new User(req.body)
     user.save((err, userInfo) => {
-        if(err) return rew.json({ success: false, err })
+        if(err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
         })
@@ -52,17 +53,38 @@ app.post('/login', (req, res) => {
         user.generateToken((err, user) => {
             if(err) return res.status(400).send(err);
             //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
-            res.cookie('x-auth', user.token)
+            res.cookie("x_auth", user.token)
             .status(200)
             .json({ loginSuccess: true, userId : user._id})
 
 
+            })
         })
     })
-
-    })
-
-
 })
+
+app.get('api/users/auth', auth ,(req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 authentication 이 true라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, 
+    (err, user) => {
+        if(err) return res.json({ success: false, err });
+        return res.status(200).send({success: true})
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -35,7 +35,7 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.pre('save', function( next ) {
+userSchema.pre('save', function(next) {
     var user = this;
     if(user.isModified('password')) {
     //비밀번호를 암호화 시킨다.
@@ -69,6 +69,20 @@ userSchema.methods.generateToken = function(cb) {
     user.save(function(err, user) {
         if(err) return cb(err);
         cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    //토큰 디코드하기
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 토큰과 DB에서 보관된 토큰이 일치하는 지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user)
+        })
     })
 }
 
